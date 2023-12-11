cc29c7e83c31e7425feb139f2782ed19dacfe576
B e n g i <bambang.trd17@gmail.com>
2023-12-11 08:59:24 +0700

    Part 1 Setting Up SwiftUI Project & App Lifecycle

---
+++ Football Chants/Football Chants/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football Chants
+//
+//  Created by Bambang Tri Rahmat Doni on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football Chants/Football Chants/Football_ChantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_ChantsApp.swift
+//  Football Chants
+//
+//  Created by Bambang Tri Rahmat Doni on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Football_ChantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
