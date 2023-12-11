2cdf0bb4af6628286ea16e68b6710b4a41c31dcb
B e n g i <bambang.trd17@gmail.com>
2023-12-11 10:09:32 +0700

    Part 4 Working with the Interface Builder

--- Football Chants/Football Chants/Views/TeamView.swift
+++ Football Chants/Football Chants/Views/TeamView.swift
@@ -10,15 +10,29 @@ import SwiftUI
 struct TeamView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            self.helloView()
         }
-        .padding()
     }
 }
 
 #Preview {
     TeamView()
 }
+
+private extension TeamView {
+    @ViewBuilder
+    private func helloView() -> some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Football Chants!")
+        }
+        .font(.headline)
+        .padding()
+        .background(Color.gray.opacity(0.3))
+        .clipShape(RoundedRectangle(cornerRadius: 15.0))
+        .shadow(radius: 5.0)
+    }
+}
+
